45dcbfba230757aac9bd23ad6670c4cf2297463f
Ivan Andrić <andricivan86@gmail.com>
2021-12-21 15:04:21 +0100

    renamed

--- logic/config.py
+++ logic/config.py
@@ -2,6 +2,6 @@ import json
 
 
 def get_environment_variables():
-    with open(r'C:\Users\Ivan\PycharmProjects\realEstateProperties\envVar.json', 'r') as content:
+    with open(r'C:\Users\Ivan\PycharmProjects\realEstateProperties\credentials.json', 'r') as content:
         env_var_value = json.load(content)
     return env_var_value
